48dd2c96ee9762aa07289985b4739b0f062f7e91
Pedro Gabriel <gaboleal123@gmail.com>
2020-12-10 20:15:48 -0400

    login

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'Auth\LoginController@showLoginForm')->name('login');
-
+Route::get('/', function () {
+    return view('auth.login');
+});
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
